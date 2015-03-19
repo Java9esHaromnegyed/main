655e56eeff7fc1972c2c094986beeaa046d56a38
Richard Balog <richard@oktafone.com>
2015-03-20 00:29:57 +0100

    Levi

--- Main/src/com/szoftlab4/java9esharomnegyed/Game.java
+++ Main/src/com/szoftlab4/java9esharomnegyed/Game.java
@@ -12,11 +12,16 @@ public class Game {
     private static Clock clock;
 
     public static void main(String[] args) {
+        System.out.println("main()");
+        System.out.println("menu(); main()");
         menu();
     }
 
     public static void requestName(String name, int player) {
+        System.out.println("RequestName()");
+        System.out.println("setRobotName(); requestName()");
         arena.setRobotName(name, player);
+        System.out.println("requestName() returned with: void");
     }
 
     public static void showLeaderboard() {
@@ -26,6 +31,7 @@ public class Game {
     }
 
     public static void menu() {
+        System.out.println("menu()");
         System.out.println("[1] New Game");
         System.out.println("[2] Robot Movement");
         System.out.println("[3] Leave Game");
@@ -38,29 +44,38 @@ public class Game {
             String menuOption = bufferedReader.readLine();
             int option = Integer.parseInt(menuOption);
             switch (option) {
-                case 1 : newGame();
+                case 1 : System.out.println("newGame(); menu()");
+                        newGame();
                          break;
-                case 2 : newGame();
+                case 2 : System.out.println("newGame(); menu()");
+                        newGame();
                         arena.movementControl(new Event(null, KeyEvent.VK_W, KeyEvent.VK_W));
                         break;
-                case 3 : pauseGame();
+                case 3 : System.out.println("pauseGame(); menu()");
+                        pauseGame();
                         break;
-                case 0 : exitGame();
+                case 0 : System.out.println("exitGame(); menu()");
+                        exitGame();
                         break;
                 default : throw new Throwable();
             }
         } catch (Throwable e) {
             System.out.println("Invalid menu argument");
         }
+        System.out.println("menu() returned with: void");
     }
 
     public static void newGame(){
+        System.out.println("Arena(); newGame()");
         arena = new Arena();
         leaderborad = new Leaderborad();
         clock = new Clock();
 
+        System.out.println("requestGame(); newGame()");
         requestName("player1", 0);
+        System.out.println("requestName(); newGame()");
         requestName("player2", 1);
+        System.out.println("newGame() returned with: void");
     }
 
     public static void pauseGame(){
@@ -79,15 +94,20 @@ public class Game {
 
     public static void gameOver(){
         clock.stopClock();
+        System.out.println("menu(); gameOver()");
         menu();
+        System.out.println("gameOver() returned with: void");
     }
 
     public static void leaveGame(){
         clock.stopClock();
+        System.out.println("menu(); leaveGame()");
         menu();
+        System.out.println("leaveGame() returned with: void");
     }
 
     public static void exitGame(){
+        System.out.println("Kilépés");
         System.exit(0);
     }
 }
