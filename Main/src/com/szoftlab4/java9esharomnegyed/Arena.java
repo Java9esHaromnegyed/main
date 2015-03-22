68650ce5cb835f55f6f66f77b3022de8e816ee55
Richard Balog <richard@oktafone.com>
2015-03-22 22:46:09 +0100

    Ricsi - Arena commented

--- Main/src/com/szoftlab4/java9esharomnegyed/Arena.java
+++ Main/src/com/szoftlab4/java9esharomnegyed/Arena.java
@@ -8,14 +8,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Arena {
+    //Arena osztály mezői
     private List<Obstacle> obstacles;   // a List where the arena register all the placed Obstacle
     private Dimension size;             // Arena size
     private Robot robot1;
     private Robot robot2;
 
+    //Üres konstruktor az elemek megfelelő inicializálásához
     public Arena(){
         LogHelper.call("Arena()");
+        //64x64-es pálya lesz
         size = new Dimension(64, 64);
+        //Két robot létrehozása default névvel
         robot1 = new Robot(this, "player_one", new Dimension(16, 24), Config.DIR_RIGHT);
         robot2 = new Robot(this, "player_two", new Dimension(16, 16), Config.DIR_RIGHT);
 
@@ -34,6 +38,7 @@ public class Arena {
     public void refresh() {
     }
 
+    //Robot nevét beállító fgv
     public void setRobotName(String name, int player){
         LogHelper.call("setRobotName(" + name + ", " + player + "); Arena;");
         switch (player) {
@@ -45,42 +50,50 @@ public class Arena {
         LogHelper.ret("setRobotName(name, player) lefutott;");
     }
 
+    //Egy adott pozíció alapján esetleges ott lévő akadály elkérése
     public Obstacle getObstacle(Dimension dest) {
         System.out.println("getObstacle(); Arena");
         System.out.println("getObstacle() lefutott");
         return null;
     }
 
+    //Akadály felvétele a listába
     public void addObstacle(Obstacle o) {
         System.out.println("addObstacle() -> add(o); Arena");
         obstacles.add(o);
         System.out.println("addObstacle() lefutott");
     }
 
+    //Ütközésdetekció
     public Obstacle collision(Robot r, Dimension d) {
         LogHelper.call("collision(); Arena");
         r.getPositon();
-        //TODO: ezzel miafasz legyen?
+        //TODO: ezzel mi legyen?
         LogHelper.ret("collision() lefutott");
         return null;
     }
 
+    //Pályáról kilépés érzékelése
     public boolean isOutOfArena(Dimension d) {
         LogHelper.call("isOutOfArena(); Arena");
         LogHelper.ret("isOutOfArena() lefutott");
         return false;
     }
 
+    //Effekt érvényesítése egy adott roboton egy adott pozícióban
     public void takeEffect(Robot r, Dimension position) {
         LogHelper.call("takeEffect(); Arena");
         getObstacle(position).effect(r);
         LogHelper.ret("takeEffect() lefutott");
     }
 
+    //Irányításkezelő fgv
     public void movementControl(int e) {   //Key event e
         LogHelper.call("movementControl(); Arena");
         //int key = e.getKeyCode();
         int key = e;
+        //A két robot mozgását külön gombokkal kezeljük, ezek megnyomása közvetlen a robot
+        //megfelelő fgvének meghívását vonja maga után
         switch (key) {
             case Config.MOV_P1_UP:
                 robot1.speedUp();
