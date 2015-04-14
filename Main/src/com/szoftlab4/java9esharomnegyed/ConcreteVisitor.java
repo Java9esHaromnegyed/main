29b5296d9610766841d0a78bfe468b1a2b5bb925
SoulEvans07@github.com <SoulEvans07@github.com>
2015-04-14 17:07:38 +0200

    Visitor Ádám (Soul.)

--- Main/src/com/szoftlab4/java9esharomnegyed/ConcreteVisitor.java
+++ Main/src/com/szoftlab4/java9esharomnegyed/ConcreteVisitor.java
@@ -2,22 +2,19 @@ package com.szoftlab4.java9esharomnegyed;
 
 import com.szoftlab4.java9esharomnegyed.Interface.Visitor;
 
-/**
- * Created by Ricsard on 2015.04.14..
- */
 public class ConcreteVisitor implements Visitor {
     @Override
-    public void effect(PuttySpot o) {
+    public void effect(PuttySpot p, Robot r) {
         System.out.println("putty");
     }
 
     @Override
-    public void effect(OilSpot o) {
+    public void effect(OilSpot o, Robot r) {
         System.out.println("oil");
     }
 
     @Override
-    public void effect(Wall w) {
+    public void effect(Wall w, Robot r) {
         System.out.println("wall");
     }
 }
